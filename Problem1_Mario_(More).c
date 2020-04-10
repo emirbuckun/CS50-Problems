6d0eca69e5a0b834541519503cde92f85178e935
Emir <63044078+emirbuckun@users.noreply.github.com>
2020-04-10 21:01:59 +0300

    Update and rename Problem_Set2 to Problem1_Mario_(More).c

---
+++ Problem1_Mario_(More).c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int get_positive_int(string prompt);
+
+int main(void)
+{
+    int i = get_positive_int("Height: ");
+
+    for (int w = 1; w <= i; w++)
+    {
+        for (int k = i - 1; k >= w; k--)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= w; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
+int get_positive_int(string prompt)
+{
+    int n;
+    do
+    {
+        n = get_int("%s", prompt);
+    }
+    while (n < 1 || n > 8);
+    return n;
+}
