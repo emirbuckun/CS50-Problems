564479c72190edf471e5fdb34e7c513c58098911
Emir <63044078+emirbuckun@users.noreply.github.com>
2020-04-13 12:22:34 +0300

    Solution of cash problem was created.

---
+++ Problem1/Cash(Less).c
@@ -0,0 +1,32 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int change(int cents);
+
+float get_positive_float(string prompt);
+
+int main(void)
+{
+    float amount = get_positive_float("Change owed: ");
+    
+    while (amount < 0);
+    
+    printf("%d\n", change((int)round(amount * 100)));
+}
+
+int change(int cents)
+{
+    return cents / 25 + (cents % 25) / 10 + ((cents % 25) % 10) / 5 + ((cents % 25) %10) % 5;
+}
+
+float get_positive_float(string prompt)
+{
+    float n;
+    do
+    {
+        n = get_float("%s", prompt);
+    }
+    while (n <= 0);
+    return n;
+}
