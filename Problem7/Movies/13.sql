b9143691df8c638bb1426e4ea94453490fec9498
Emir <63044078+emirbuckun@users.noreply.github.com>
2020-06-11 16:01:33 +0300

    Create 13.sql

---
+++ Problem7/Movies/13.sql
@@ -0,0 +1,2 @@
+-- a SQL query to list the names of all people who starred in a movie in which Kevin Bacon also starred.
+SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id IN (SELECT movie_id FROM stars WHERE person_id == (SELECT id FROM people WHERE name == "Kevin Bacon" AND birth == 1958))) AND name != "Kevin Bacon";
