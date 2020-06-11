420277dcf308a3577c388d8ae654045643696d7d
Emir <63044078+emirbuckun@users.noreply.github.com>
2020-06-11 16:00:22 +0300

    Create 8.sql

---
+++ Problem7/Movies/8.sql
@@ -0,0 +1,2 @@
+-- write a SQL query to list the names of all people who starred in Toy Story.
+SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_id == (SELECT id FROM movies WHERE title == "Toy Story"));
